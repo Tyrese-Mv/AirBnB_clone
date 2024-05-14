450d7ff03ad83c47a4f04b42d9c89af937370224
Tyrese-Mv <musawenkosi106@gmail.com>
2024-05-14 17:25:53 +0200

    modified base model constructer

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,20 @@ import datetime
 
 class BaseModel:
     """BaseModel Class"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) > 0:
+            if "__class__" in kwargs:
+                del kwargs["__class__"]
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+
 
     def __str__(self):
         """returns a string representation of the class
